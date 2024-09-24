44e7b57a0055d25fae307fa7baa52bed7942ffb6
Aman <jainaman9003@gmail.com>
2024-09-24 14:12:02 +0530

    other design

---
+++ Singleton Design Pattern/Problem.cpp
@@ -0,0 +1,5 @@
+/*
+    A singleton pattern is a design pattern that ensures that only one instance of a class can exist in the entire program. 
+    This means that if you try to create another instance of the class,it will return the same instance that was created earlier.
+
+*/
